d0f2ad2400488122c0fd4c6191b481b20c017c57
Caio Carvalho <caiocarvalho.py@gmail.com>
2024-05-19 14:48:05 -0400

    handlers and formatting

---
+++ file_formatting.py
@@ -0,0 +1,7 @@
+from loguru import logger
+
+fmt = "{time} - {name} - {level} - {message}"
+
+logger.add("formatted.log", format=fmt, level="INFO")
+logger.debug("This is a debug message")
+logger.info("This is an information message")

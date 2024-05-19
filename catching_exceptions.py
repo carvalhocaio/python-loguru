bf0c3edfa0ae15567db15f0ba047ed00b4622d41
Caio Carvalho <caiocarvalho.py@gmail.com>
2024-05-19 19:19:29 -0400

    catching exceptions

---
+++ catching_exceptions.py
@@ -0,0 +1,18 @@
+from loguru import logger
+
+
+@logger.catch
+def silly_function(x, y, z):
+    return 1 / (x + y + z)
+
+
+def main():
+    fmt = "{time:HH:mm:ss} - {name} - {level} - {message}"
+    logger.add("exception.log", format=fmt, level="INFO")
+    logger.info("Application starting")
+    silly_function(0, 0, 0)
+    logger.info("Application finished")
+
+
+if __name__ == "__main__":
+    main()
